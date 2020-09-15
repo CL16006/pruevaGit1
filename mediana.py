2b3193aeebff7b7e1c2c082ec5781e6e46528864
Fatima Calderon <cl16006@ues.edu.sv>
2020-09-15 17:38:08 -0600

    mediana de tres numeros

---
+++ mediana.py
@@ -0,0 +1,8 @@
+numero1=int(input('Ingrese primer numero'))
+numero2=int(input('Ingrese segundo numero'))
+numero3=int(input('ingrese tercer numero'))
+
+conjunto=[numero1,numero2,numero3]
+
+conjunto.sort()
+print('La mediana es: ',conjunto[1])
